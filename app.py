878c1b53734c7880683ec970e89613318ea0b7db
= <=>
2026-06-05 11:02:39 +0530

    Initial commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 	return jsonify({
-			"message": "Flask running with Docker compose",
+			"message": "Updated Version V2",
 			"service": "api"
 		})
 @app.route("/status")
